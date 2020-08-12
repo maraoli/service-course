5a07f81e814feaa9017f9af87cfc8726e3bb4cb6
Mara <mara.15oliveira@outlook.com>
2020-08-12 11:38:52 -0300

    recebendo usuários #5

--- node/event/liveUsersUpdate.ts
+++ node/event/liveUsersUpdate.ts
@@ -1,3 +1,9 @@
-export async function updateLiveUsers() {
+import { Clients } from '../clients/index'
+import { EventContext } from '@vtex/api'
+
+export async function updateLiveUsers(ctx: EventContext<Clients>) {
     console.log('EVENT HANDLER: received event')
+    const liveUsersProducts = await ctx.clients.analytics.getLiveUsers()
+    console.log('LIVE USERS: ', liveUsersProducts)
+    return true
 }
